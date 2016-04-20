da7918fee3360e767ab3717dec596e14f34ad659
Florian Pargent <florian.pargent@yahoo.com>
2016-04-20 09:24:46 +0200

    Added Scripts and Data

---
+++ AnalysismlR.R
@@ -0,0 +1,65 @@
+# load packages
+library(dplyr)
+library(BBmisc)
+library(ggplot2)
+library(GGally)
+library(mlr)
+
+# load dataset
+train <- read.csv(file.path("data", "train.csv"), 
+                  stringsAsFactors = FALSE, na.strings = "")
+test <- read.csv(file.path("data", "test.csv"),
+                 stringsAsFactors = FALSE, na.strings = "")
+
+# add Survived variable to test
+test$Survived <- factor(rep("NULL", nrow(test)), levels = c(0, 1))
+
+# check missing values
+vnapply(train, function(x)sum(is.na(x)))
+vnapply(test, function(x)sum(is.na(x)))
+
+# source preprocessing function
+source("preprocmlR.R")
+
+# preprocess data
+train <- preprocmlR(train)
+test <- preprocmlR(test)
+
+# pairs plot
+pdf("pairsPlot.pdf", height = 20, width = 20)
+pairsPlot <- train %>% ggpairs
+print(pairsPlot)
+dev.off()
+
+# descriptive tables
+freq <- function(x)sum(x == 1)/length(x)
+train %>% na.omit %>% group_by(Pclass, Age_cat, Sex) %>% 
+  summarise(n(), freq(Survived)) %>% as.data.frame
+
+## predictive modelling in mlr:
+# make task
+classif_task <- makeClassifTask(id = "Titanic", data = train, 
+                                target = "Survived", positive = "1")
+
+# make learner
+logreg_lrn <- makeLearner("classif.logreg", predict.type = "prob", 
+                          fix.factors.prediction = TRUE)
+rforest_lrn <- makeLearner("classif.randomForest", predict.type = "prob",
+                          fix.factors.prediction = TRUE)
+# create impute wrapper for logreg: impute numeric by mean/factor by mode
+logreg_wrap <- makeImputeWrapper(logreg_lrn, 
+                                 cols = list(Fare = imputeMean(),
+                                                Embarked = imputeMode(),
+                                                Age_cat = imputeMode()))
+rforest_wrap <- makeImputeWrapper(rforest_lrn, 
+                                 cols = list(Fare = imputeMean(),
+                                             Embarked = imputeMode(),
+                                             Age_cat = imputeMode()))
+# resample description
+rdescr <- makeResampleDesc("CV", iters = 10)
+
+# resampled logreg
+logreg_res <- resample(logreg_wrap, classif_task, rdescr)
+rforest_res <- resample(rforest_wrap, classif_task, rdescr)
+# more clean combination
+bm <- benchmark(list(logreg_wrap, rforest_wrap), classif_task, rdescr)
---
+++ preprocmlR.R
@@ -0,0 +1,9 @@
+preprocmlR <- function(df){
+  # creat categorical Age variable
+  df$Age_cat <- cut(df$Age, breaks = c(0, 18, 55, 90))
+  # remove unused variables and convert to factors
+  df <- df %>% mutate_each(funs(factor), 
+                           c(Survived, Pclass, Sex, Embarked)) %>%
+    select(-c(PassengerId, Name, Ticket, Cabin, Age))
+  return(df)
+}
