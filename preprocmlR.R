ad5d104212d6ee8998c5b0105ba6c2c6f7799c7f
Felix <felix_n565@yahoo.de>
2016-04-20 12:07:50 +0200

    delete source code

--- preprocmlR.R
+++ preprocmlR.R
@@ -1,8 +1,8 @@
 preprocmlR <- function(df){
-  # creat categorical Age variable
+  # create categorical Age variable
   df$Age_cat <- cut(df$Age, breaks = c(0, 18, 55, 90))
   # remove unused variables and convert to factors
-  df <- df %>% mutate_each(funs(factor), 
+  df <- df %>% mutate_each(funs(factor),
                            c(Survived, Pclass, Sex, Embarked)) %>%
     select(-c(PassengerId, Name, Ticket, Cabin, Age))
   return(df)
