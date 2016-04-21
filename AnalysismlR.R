272cb8f4a4d4d69e08e4ba17d894eff967750d23
Florian Pargent <florian.pargent@yahoo.com>
2016-04-21 08:18:42 +0200

    write predictions for kaggle and remove preprocessing for testdataframe (mlR can handle that)

--- AnalysismlR.R
+++ AnalysismlR.R
@@ -11,9 +11,6 @@ train <- read.csv(file.path("data", "train.csv"),
 test <- read.csv(file.path("data", "test.csv"),
                  stringsAsFactors = FALSE, na.strings = "")
 
-# add Survived variable to test
-test$Survived <- factor(rep("NULL", nrow(test)), levels = c(0, 1))
-
 # check missing values
 vnapply(train, function(x)sum(is.na(x)))
 vnapply(test, function(x)sum(is.na(x)))
@@ -23,7 +20,6 @@ source("preprocmlR.R")
 
 # preprocess data
 train <- preprocmlR(train)
-test <- preprocmlR(test)
 
 # pairs plot
 pdf("pairsPlot.pdf", height = 20, width = 20)
@@ -58,6 +54,7 @@ rforest_wrap <- makeImputeWrapper(rforest_lrn,
 logreg_mod <- train(logreg_wrap, classif_task)
 summary(getLearnerModel(logreg_mod, more.unwrap = TRUE))
 rforest_mod <- train(rforest_wrap, classif_task)
+
 getLearnerModel(rforest_mod, more.unwrap = TRUE)$importance
 getLearnerModel(rforest_mod, more.unwrap = TRUE)$confusion
 
@@ -71,3 +68,16 @@ rforest_res <- resample(rforest_wrap, classif_task, rdescr)
 # more clean solution
 bm <- benchmark(list(logreg_wrap, rforest_wrap), classif_task, rdescr)
 
+# make predictions
+logreg_pred <- predict(logreg_mod, newdata = test)
+rforest_pred <- predict(rforest_mod, newdata = test)
+
+# write csv predictions for kaggle
+logreg_pred_kaggle <- test %>% 
+  mutate(Survived = as.data.frame(logreg_pred)$response) %>% 
+  select(PassengerId, Survived)
+rforest_pred_kaggle <- test %>% 
+  mutate(Survived = as.data.frame(rforest_pred)$response) %>% 
+  select(PassengerId, Survived)
+write.csv(logreg_pred_kaggle, "logreg_pred.csv", row.names = FALSE)
+write.csv(rforest_pred_kaggle, "rforest_pred.csv", row.names = FALSE)
